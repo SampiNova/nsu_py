8e512efebef86fa1ba20910f46df48ea26cd8f06
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-09-04 14:12:26 +0700

    origin

--- lesson3/lesson3.py
+++ lesson3/lesson3.py
@@ -14,6 +14,42 @@ class Item:
     def count(self):
         return self._count
 
+    def __add__(self, num):
+        return self._count + num
+
+    def __sub__(self, num):
+        return self._count - num
+
+    def __mul__(self, num):
+        return self._count * num
+
+    def __iadd__(self, num):
+        self._count += num
+
+    def __isub__(self, num):
+        self._count -= num
+
+    def __imul__(self, num):
+        self._count *= num
+
+    def __lt__(self, num):
+        return self._count < num
+
+    def __gt__(self, num):
+        return self._count > num
+
+    def __le__(self, num):
+        return self._count <= num
+
+    def __ge__(self, num):
+        return self._count >= num
+
+    def __eq__(self, num):
+        return self._count == num
+
+    def __len__(self):
+        return self._count
+
 
 class Fruit(Item):
     def __init__(self, ripe=True, **kwargs):
@@ -29,3 +65,17 @@ class Food(Item):
     @property
     def eatable(self):
         return self._saturation > 0
+
+
+class Kiwi(Fruit, Food):
+    def __init__(self, meat, ripe, count=1, max_count=50, saturation=5):
+        super().__init__(ripe=ripe, count=count, max_count=max_count, saturation=saturation)
+        self.meat = meat
+
+    @property
+    def meat(self):
+        return self.meat
+
+    @property
+    def eatable(self):
+        return super().eatable and self._ripe
