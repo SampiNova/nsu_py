482a15002250fce68b1288bbb250bcb5455455ea
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-09-06 12:42:30 +0700

    origin

--- lesson4/lesson4.py
+++ lesson4/lesson4.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+# 1
+np.random.seed(18182)
+a = np.random.randint(5, size=20)
+nums, freq = np.unique(a, return_counts=True)
+nums = list(nums)
+print(a)
+print(np.array(sorted(a, key=lambda x: int(freq[nums.index(x)]))))
