5be9a52a88669e791dc87db89b09ef96bb635390
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-09-07 14:18:04 +0700

    origin

--- lesson4/lesson4.py
+++ lesson4/lesson4.py
@@ -7,3 +7,28 @@ nums, freq = np.unique(a, return_counts=True)
 nums = list(nums)
 print(a)
 print(np.array(sorted(a, key=lambda x: int(freq[nums.index(x)]))))
+
+# 2
+h, w = 10, 20
+image = np.array(np.random.randint(255, size=(h, w)), dtype=np.uint8)
+print(len(np.unique(image)))
+
+# 3
+vec = np.random.randint(125, size=10)
+
+
+def func(v, n):
+    summ = np.cumsum(np.insert(vec, 0, 0))
+    return (summ[n:] - summ[:-n]) / n
+
+
+print(vec)
+print(func(vec, 3))
+
+# 4
+n = 30
+inp = np.random.randint(180, size=(n, 3))
+lam = (lambda y: y[0] + y[1] > y[2] and
+                 y[0] + y[2] > y[1] and
+                 y[1] + y[2] > y[0])
+print(list(filter(lam, inp)))
