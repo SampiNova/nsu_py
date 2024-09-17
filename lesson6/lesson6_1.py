2390749c4db8b45007ee1fa5a2ce7ecaed1b332a
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-09-17 11:21:57 +0700

    origin

--- lesson6/lesson6_1.py
+++ lesson6/lesson6_1.py
@@ -6,7 +6,24 @@ import os
 
 
 def key_press_event(event: plt_bb.KeyEvent):
-    print(event.key)
+    global axis11, axis12, axis21, axis22, idx, pairs_of_images
+    if event.key == "right":
+        idx += 1
+    elif event.key == "left":
+        idx -= 1
+    else:
+        return
+    if idx >= len(pairs_of_images):
+        idx = 0
+    if idx < 0:
+        idx = len(pairs_of_images) - 1
+
+    axis11.imshow(pairs_of_images[idx][0])
+    axis12.imshow(pairs_of_images[idx][2])
+    axis21.imshow(pairs_of_images[idx][1], cmap="gray")
+    axis22.imshow(pairs_of_images[idx][3], cmap="gray")
+
+    axis11.figure.canvas.draw()
 
 
 images_path = "nails_segmentation\\images"
@@ -16,33 +33,25 @@ labels_path = "nails_segmentation\\labels"
 def read_pairs():
     pairs = []
     for image_name in os.listdir(images_path):
-        image = cv2.imread(images_path + "\\" + image_name)
+        image = cv2.imread(images_path + "\\" + image_name)[:, :, ::-1]
         label = cv2.imread(labels_path + "\\" + image_name)
-        pairs += [(image, label)]
+        label = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
+        contours, hierarchy = cv2.findContours(label, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        cont_image = cv2.drawContours(image.copy(), contours, -1, (0, 255, 0), 2)
+        cont_label = cv2.drawContours(np.zeros(label.shape), contours, -1, (255, 255, 255), 2)
+        pairs += [(image, label, cont_image, cont_label)]
     return pairs
 
 
 idx = 0
 if __name__ == "__main__":
     pairs_of_images = read_pairs()
-    fig, (axis1, axis2) = plt.subplots(nrows=2)
+    fig, ((axis11, axis12), (axis21, axis22)) = plt.subplots(nrows=2, ncols=2)
 
-    axis1.imshow(pairs_of_images[idx][0])
-    axis1.grid()
-    axis2.imshow(pairs_of_images[idx][1])
-    axis2.grid()
+    axis11.imshow(pairs_of_images[idx][0])
+    axis12.imshow(pairs_of_images[idx][2])
+    axis21.imshow(pairs_of_images[idx][1], cmap="gray")
+    axis22.imshow(pairs_of_images[idx][3], cmap="gray")
 
     key_press_event_id = fig.canvas.mpl_connect("key_press_event", key_press_event)
     plt.show()
-
-'''fig1, _ = plt.subplot(1, 2, 1)
-plt.imshow(pairs[0][0])
-fig2, _ = plt.subplot(1, 2, 2)
-plt.imshow(pairs[0][1])
-
-key_press_event_id_1 = fig1.canvas.mpl_connect("key_press_event", key_press_event)
-key_press_event_id_2 = fig2.canvas.mpl_connect("key_press_event", key_press_event)
-
-plt.show()
-fig1.canvas.mpl_disconnect(key_press_event_id_1)
-fig2.canvas.mpl_disconnect(key_press_event_id_2)'''
