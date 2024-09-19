1c0c90b93983c6332e4e06d367b90da80410f4bb
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-09-19 15:25:06 +0700

    origin

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -6,8 +6,7 @@ cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     key = cv2.waitKey(20) & 0xff
-    # img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    kernel = np.
+    img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     cv2.imshow('Frame', img)
     if key == 27:
         break
