556579b8ae4b5b8ad0731d8a6cac802c069cefd7
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-09-20 17:42:30 +0700

    origin

--- lesson6/lesson6_2.py
+++ lesson6/lesson6_2.py
@@ -1,11 +1,15 @@
 import cv2
 
+mnr, mxr, mmr = 0.06274509803921569, 0.15294117647058825, 0.08617110983455897
+mng, mxg, mmg = 0.0, 0.11372549019607843, 0.08060166245404368
+mnb, mxb, mmb = 0.0, 0.611764705882353, 0.06439780394709965
+
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     key = cv2.waitKey(20) & 0xff
-    img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     cv2.imshow('Frame', img)
     if key == 27:
         break
--- test.py
+++ test.py
@@ -2,20 +2,81 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
+
+def analysis(image_):
+    global n
+    size_ = image_.shape[0] * image_.shape[1]
+    red_data = {"min": np.min(image_[:, :, 2]),
+                "max": np.max(image_[:, :, 2]),
+                "mean": np.mean(image_[:, :, 2]),
+                "pixels": image_[120, 120, 2],
+                "count": n,
+                "non_zero": np.count_nonzero(image_[:, :, 2])}
+    green_data = {"min": np.min(image_[:, :, 2]),
+                  "max": np.max(image_[:, :, 2]),
+                  "mean": np.mean(image_[:, :, 2]),
+                  "pixels": image_[120, 120, 1],
+                  "count": n,
+                  "non_zero": np.count_nonzero(image_[:, :, 2])}
+    blue_data = {"min": np.min(image_[:, :, 2]),
+                 "max": np.max(image_[:, :, 2]),
+                 "mean": np.mean(image_[:, :, 2]),
+                 "pixels": np.reshape(image_[:, :, 2], (n, 1)),
+                 "count": n,
+                 "non_zero": np.count_nonzero(image_[:, :, 2])}
+    return red_data, green_data, blue_data
+
+
+def furry(ys, N_):
+    w = np.exp(-2 * np.pi * 1j / N_)
+    W = np.fromfunction(lambda x, y: w ** (x * y), (N_, N_))
+    return (1 / np.sqrt(N_)) * W @ ys
+
+
+# ==============================================================================
+MIN = 0.06274509803921569
+MAX = 0.1450980392156863
+MEAN = 0.08654021675857848
+# ==============================================================================
 cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+
+n = 100
+width, height, _ = frame.shape
 
 frames = []
+a, b = 2, 2
+step_a, step_b = width // (a + 2), height // (b + 2)
+reds = np.zeros((n, a * b))
 
-n = 50
-for _ in range(n):
+for k in range(n):
     ret, frame = cap.read()
     frames.append(frame)
+    for i in range(a * b):
+        reds[k, i] = (frame[step_a * (i % a + 1), step_b * (i // a + 1), 2])
 frames = np.array(frames)
+frames = frames / 255
+reds = reds / 255
 
+cap.release()
 
-reds = frames[:, :, :, 2]
-shp_r = reds.shape
-reds = reds.reshape((1, shp_r[0] * shp_r[1] * shp_r[2]))
-print(reds)
+fig, (axis1, axis2) = plt.subplots(ncols=2)
+
+ox = np.linspace(0.0, n - 1, n)
+mean = np.sum(frames, axis=0) / n
+print(np.min(frames[:, :, :, 2]), np.max(frames[:, :, :, 2]), np.mean(frames[:, :, :, 2]))
+print(np.min(frames[:, :, :, 1]), np.max(frames[:, :, :, 1]), np.mean(frames[:, :, :, 1]))
+print(np.min(frames[:, :, :, 0]), np.max(frames[:, :, :, 0]), np.mean(frames[:, :, :, 0]))
+axis2.plot(ox, (reds - 2 * MEAN + MIN) / (MAX - MIN) + 1.0)
+
+# f = furry(R["pixels"], n)
+# ox = np.linspace(0.0, n - 1, n)
+
+'''fig, axiss = plt.subplots(ncols=a, nrows=b)
+print(axiss)
+for i in range(b):
+    for j in range(a):
+        axiss[i][j].plot(ox, reds[:, 2 * i + j])
+        axiss[i][j].grid()'''
 
 plt.show()
