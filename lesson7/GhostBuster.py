fc796bb50366901360705c444430e6bacf1d8f92
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-10-15 04:18:46 +0700

    new

--- lesson7/GhostBuster.py
+++ lesson7/GhostBuster.py
@@ -3,4 +3,14 @@ import cv2
 import os
 import matplotlib.pyplot as plt
 
+dir_path = "ghosts"
+image = cv2.imread(f"{dir_path}\\candy_ghost.png")
 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+sift = cv2.ORB_create()
+kp = sift.detect(gray, None)
+
+result = image.copy()
+result = cv2.drawKeypoints(gray, kp, result)
+plt.imshow(result, cmap='gray')
+plt.show()
