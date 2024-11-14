f242527799c880c5a662ccdcf119b6b62c7a65fb
Dmitry-py <dmitry.karpachev@hotmail.com>
2024-11-14 14:21:07 +0700

    updated

--- math/my_math.py
+++ math/my_math.py
@@ -2,15 +2,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sympy as sp
 from PIL.ImageOps import expand
+from sympy import simplify
+
+
+def simp_sq(sq_a, sq_b):
+    f = [0, 0, 0]
+    s = 0
+    for i in range(3):
+        if sq_b[i] != 0:
+            if sq_a[i] != 0:
+                f[i] = sq_b / sq_a / 2
+                s += f[i] ** 2 * sq_a
+    return f, s
+
+def simp_pr(sq_a, sq_b, sq_c):
+    return -sq_b / sq_a, sq_c / sq_b
+
 
 # Общее уравнение поверхности второго порядка в матричном виде
 # X.T @ A @ X + 2 * b.T * X + c = 0
 
-'''
+
 Id = sp.eye(3)
 o = np.matrix([[0], [0], [0]], dtype=np.float64)
 
-a11, a22, a33 = with_sqX = np.array([1, 0, 0])  # a11 a22 a33
+a11, a22, a33 = with_sqX = np.array([1, 0, 0], dtype=np.float64)  # a11 a22 a33
 a12, a13, a23 = with_rotX = np.array([0, 0, 0]) / 2  # a12(xy) a13(xz) a23(yz)
 b1, b2, b3 = with_linX = np.array([0, 0, -8]) / 2  # b1 b2 b3
 c = 10
@@ -18,6 +34,9 @@ c = 10
 A = sp.Matrix([[a11, a12, a13],
                [a12, a22, a23],
                [a13, a23, a33]])
+b = sp.Matrix([b1, b2, b3])
+cm = sp.Matrix([c])
+
 bigA = sp.Matrix([[a11, a12, a13, b1],
                   [a12, a22, a23, b2],
                   [a13, a23, a33, b3],
@@ -52,94 +71,31 @@ else:
 
 print(d, D, D_, T, S)
 
-a = sp.symbols("a")
-eq = sp.Eq(a ** 3 - S * a ** 2 + T * a - d, 0)
-print(sp.roots(eq))
+A = sp.Matrix([[a11, a12, a13], [a12, a22, a23], [a13, a23, a33]])
 
-x, y, z, w = sp.symbols("x y z w")
-eq = sp.Eq(sp.Matrix([x * y * z * w,
-                      x * y * z,
-                      x * y + y * z + x * z,
-                      x + y + z]), sp.Matrix([D, d, T, S]))
-_, sol = sp.solve(eq, set=True)
-sol = list(sol)
-newA = sp.Matrix(np.diag(sol[0]))
+ev = A.eigenvects()
+dg = []
+es = []
+for li in ev:
+    dg.append(li[0])
+    es.append((li[2][0] / li[2][0].norm()).T)
 
-X = sp.Matrix([x, y, z])
-
-print(X.T @ newA @ X)
-
-
-A = sp.Matrix([[1, 0, 0], [0, 0, 0], [0, 0, 0]])
-b = sp.Matrix([0, 3, -4])
-cs = 10
-c = sp.Matrix([cs])
-
-l = sp.symbols('l')
-
-det_expr = (A - l * Id).det()
-eq = sp.Eq(det_expr, 0)
-solution = sp.roots(eq)
-print(solution)
-
-match len(solution):
-    case 1:
-        S = Id
-        lam1 = lam2 = lam3 = 1
-    case 2:
-        my_root = 0
-        lam2 = 0
-        for root in solution:
-            if solution[root] == 1:
-                my_root = root
-            else:
-                lam2 = root
-        lam3 = my_root
-        lam1 = lam2
-        B = A - lam3 * Id
-        l3 = np.linalg.lstsq(np.matrix(B, dtype=np.float64), o, None)[0]
-        l3 = l3.T
-        l3 = sp.Matrix(np.matrix(l3, dtype=np.int32))
-        l2 = None
-        for i in range(B.cols):
-            if B.col(i) != 0:
-                l2 = B.col(i).T
-                break
-        l1 = l2.cross(l3)
-        l1 = l1 / l1.norm()
-        l2 = l2 / l2.norm()
-        l3 = l3 / l3.norm()
-        S = sp.Matrix([l1, l2, l3]).T
-    case 3:
-        S = Id
-        lam1 = lam2 = lam3 = 1
-    case _:
-        print("error")
-        quit()
-
-
-Lam = sp.Matrix([lam1, lam2, lam3])
 x, y, z = sp.symbols("x y z")
 X = sp.Matrix([x, y, z])
-X_sq = sp.Matrix([x ** 2, y ** 2, z ** 2])
-print(f"Your input function: {(X.T @ A @ X + 2 * b.T @ X + c).expand()[0]} = 0")
 
-b_ = S.T * b
-print((Lam.T @ X_sq + 2 * b_.T @ X + c)[0])
+A_ = sp.diag(*dg)
 
-xs = [x, y, z]
-lams = [lam1, lam2, lam3]
-bs = list(b_)
+Q = sp.Matrix(es)
+newX = Q @ X
 
-ans = cs
-for i in range(3):
-    if bs[i] != 0:
-        if lams[i] != 0:
-            ans += lams[i] * (xs[i] + bs[i] / lams[i]) ** 2 - bs[i] ** 2 / lams[i]
-        else:
-            ans += 2 * bs[i] * xs[i]
-    else:
-        ans += lams[i] * xs[i] ** 2
+print(f"Input: {(X.T @ A @ X + 2 * b.T @ X + cm)[0]} = 0")
+newF = (newX.T @ A_ @ newX + 2 * (Q.T @ b).T @ newX + cm)[0]
+print(newF)
 
-print(ans)
-'''
+if T == 0:
+    my_pol = sp.poly(newF, X)
+    print(simp_pr(*my_pol.coeffs()))
+    print(my_pol.free_symbols)
+else:
+    pass
+
